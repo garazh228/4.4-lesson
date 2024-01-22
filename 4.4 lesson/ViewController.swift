15af679b81d444b3a351422f5aa8355e9dce871d
garazh228 <adylbeksadykbekov.9.02@gmail.com>
2024-01-22 19:05:15 +0600

    Initial Commit

---
+++ 4.4 lesson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  4.4 lesson
+//
+//  Created by adyl CEO on 22/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
